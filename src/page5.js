f01b38e4f269434587ee0e0b4da9795b73599212
elisaohh <elisaohh@gmail.com>
2024-11-19 23:53:18 +0900

    하

--- src/page5.js
+++ src/page5.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 
 function Page5({ goToPage, setReceiptName, receiptItems, receiptDate }) {
-    const [name, setName] = useState(''); // 이름 상태
-    const [count, setCount] = useState(1000); // 기본값 1000부터 시작
+    const [name, setName] = useState('');
+    const [count, setCount] = useState(1000);
 
     const handleSubmit = () => {
-        const finalName = name.trim() === '' ? `User${count}` : name; // 이름이 비어있다면 'User{count}'로 설정
+        const finalName = name.trim() === '' ? `User${count}` : name;
 
         const itemsData = encodeURIComponent(JSON.stringify(receiptItems));
         const dateData = encodeURIComponent(receiptDate);
@@ -14,9 +14,8 @@ function Page5({ goToPage, setReceiptName, receiptItems, receiptDate }) {
         // 영수증 페이지로 이동
         goToPage(`receipt?items=${itemsData}&name=${nameData}&date=${dateData}`);
 
-        // count 증가
         if (name.trim() === '') {
-            setCount(prevCount => prevCount + 1); // 이름이 비어있을 때 count 증가
+            setCount(prevCount => prevCount + 1);
         }
     };
 
