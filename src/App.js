0670ec1c2000d97bba26166b289b2157f178bb3e
elisaohh <elisaohh@gmail.com>
2024-11-20 01:08:03 +0900

    대문자수정0108

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Page2 from './Page2';
 import Page3 from './Page3';
 import Page4 from './Page4';
 import Page5 from './Page5';
-import ReceiptPage from './Receipt';
+import ReceiptPage from './receipt';
 
 const App = () => {
     const navigate = useNavigate();
