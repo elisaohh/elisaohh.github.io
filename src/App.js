b82eacda5c8a6a03b5d579b1de4c60caecec4d0a
elisaohh <elisaohh@gmail.com>
2024-11-19 19:35:28 +0900

    라우터 사용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Home'; // 홈 컴포넌트
 import Page2 from './page2'; // 다른 페이지들
 import Page3 from './page3';
@@ -8,36 +9,36 @@ import ReceiptPage from './Receipt'; // 영수증 페이지 컴포넌트
 import QRCodeGenerator from './QRCodeGenerator'; // QR 코드 생성기
 
 const App = () => {
-    const [currentPage, setCurrentPage] = useState('home'); // currentPage 상태 추가
     const [receiptItems, setReceiptItems] = useState([]);
     const [receiptDate, setReceiptDate] = useState('');
     const [receiptName, setReceiptName] = useState('');
 
-    const renderPage = () => {
-        switch (currentPage) {
-            case 'home':
-                return <Home goToPage={setCurrentPage} />;
-            case 'page2':
-                return <Page2 goToPage={setCurrentPage} />;
-            case 'page3':
-                return <Page3 goToPage={setCurrentPage} setReceiptItems={setReceiptItems} />;
-            case 'page4':
-                return <Page4 goToPage={setCurrentPage} setReceiptDate={setReceiptDate} />;
-            case 'page5':
-                return <Page5 goToPage={setCurrentPage} setReceiptName={setReceiptName} />;
-            case 'receipt':
-                return <ReceiptPage items={receiptItems} name={receiptName} date={receiptDate} />;
-            case 'qrcode':
-                return <QRCodeGenerator goToPage={setCurrentPage} />;
-            default:
-                return <Home goToPage={setCurrentPage} />; // 기본 페이지
-        }
-    };
-
     return (
-        <div className="App">
-            {renderPage()}
-        </div>
+        <Router>
+            <Switch>
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+                <Route path="/page2">
+                    <Page2 />
+                </Route>
+                <Route path="/page3">
+                    <Page3 setReceiptItems={setReceiptItems} />
+                </Route>
+                <Route path="/page4">
+                    <Page4 setReceiptDate={setReceiptDate} />
+                </Route>
+                <Route path="/page5">
+                    <Page5 setReceiptName={setReceiptName} />
+                </Route>
+                <Route path="/receipt">
+                    <ReceiptPage items={receiptItems} name={receiptName} date={receiptDate} />
+                </Route>
+                <Route path="/qrcode">
+                    <QRCodeGenerator />
+                </Route>
+            </Switch>
+        </Router>
     );
 };
 
